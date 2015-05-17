b8f7559124207a2aeb5a30fb90fafefe39100afc
Charanjit Singh <ckhabra007@gmail.com>
2015-05-17 16:18:01 +0530

    Version bump

--- package.js
+++ package.js
@@ -1,8 +1,8 @@
 Package.describe({
   name: 'channikhabra:hot-loader',
-  version: '0.0.1',
-  summary: 'Package for hot-loading code while developing meteor apps. Require meteor-hot-load npm module installed globally',
-  git: '',
+  version: '0.1.0',
+  summary: 'Package for hot-loading code while developing meteor apps. Require meteor-hot-load installed globally',
+  git: 'https://github.com/channikhabra/channikhabra-hot-loader',
   documentation: 'readme.org'
 });
 
